2964b427a95415d8fb30eb4e04cb2788891ec503
Mike Rousselle <mrousselle@athenahealth.com>
2018-06-16 11:50:01 -0400

    cookie cutter DS files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='nba_metamediamodels',
+    packages=find_packages(),
+    version='0.0.1',
+    description='analysis of NBA analysis',
+    author='mrousselle',
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+)
